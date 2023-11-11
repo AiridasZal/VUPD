e810f679f1b3e9c8dabd07d6897eae2f03a71164
Airidas Žaliauskas <air.airidas1@gmail.com>
2023-11-11 17:02:00 +0200

    Initialize project

---
+++ VUPD/src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+  return (
+    <>
+      <div>HomePage</div>
+      <p className="text-3xl font-bold underline">Tailwind Test</p>
+    </>
+  );
+};
+
+export default HomePage;
